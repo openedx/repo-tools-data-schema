05e86f9906c2fccc17b2f4851066062b6300cccd
Ned Batchelder <ned@nedbatchelder.com>
2023-08-09 11:03:09 -0400

    feat!: remove validation of people.yaml
    https://github.com/openedx/openedx-webhooks/pull/251 removes people.yaml
    from the OSPR bot, so we no longer need to validate it.

--- repo_tools_data_schema/__init__.py
+++ repo_tools_data_schema/__init__.py
@@ -4,4 +4,4 @@ Schema for repo-tools-data.
 
 __version__ = '1.1'
 
-from .repo_tools_data_schema import validate_orgs, validate_people, validate_salesforce_export
+from .repo_tools_data_schema import validate_orgs, validate_salesforce_export
